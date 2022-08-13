6ea4eedccd89b809f16625bcffce8b5337b56c1e
juanDavidLEon <96037642+juanDavidLEon@users.noreply.github.com>
2022-08-13 02:50:40 -0500

    Conexion a base de datos

--- src/Servicios/Test.java
+++ src/Servicios/Test.java
@@ -76,11 +76,9 @@ public class Test extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-        
-           Conexion test = new Conexion();
-           
-            test.getConnection();
+   
+        Conexion test = new Conexion();
+        test.getConnection();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
